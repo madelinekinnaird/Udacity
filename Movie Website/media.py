f5b0e388c384f121d30d5b7fc78bd6b25e27811a
Madeline Kinnaird <madeline.kinnaird@gmail.com>
2018-06-05 10:24:35 -0500

    Update media.py

--- Movie Website/media.py
+++ Movie Website/media.py
@@ -1,10 +1,12 @@
 import webbrowser
 
+#Creating a class (movie) and using a initilizing function
 class Movie():
 	def __init__(self, movie_title, poster_image, trailer_youtube):
 		self.title = movie_title
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
-
+		
+#show trailer opens a youtube video using the url and the webbrowser feature
 	def show_trailer(self):
 		webbrowser.open(self.trailer_youtube_url)
